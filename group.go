3ccf137a957ffc62f5e51453b4aed54b1983f814
tzz <koi_town@outlook.com>
2022-05-10 12:53:55 +0800

    group.go

--- group.go
+++ group.go
@@ -1,6 +1,6 @@
 package iter
 
-func GroupAsMap[T,I comparable](it Iterator[T], groupGetter func(v T) (I) ) (map[I][]T,error){
+func GroupAsMap[T any ,I comparable](it Iterator[T], groupGetter func(v T) (I) ) (map[I][]T,error){
 	m := make(map[I][]T)
 	err := ForEach(
 			it,
@@ -19,8 +19,27 @@ func GroupAsMap[T,I comparable](it Iterator[T], groupGetter func(v T) (I) ) (map
 	return m, nil
 }
 
+func GroupAsMap_A [T any ,I comparable, V any] (it Iterator[T],groupGetter func(t T)(I,V)) (map[I][]V,error){
+	m := make(map[I][]V)
+	err := ForEach(
+			it,
+			func(t T)(error){
+				id,v := groupGetter(t)
+				if m[id] == nil{
+					m[id] = make([]V,0,1)
+				}
+				m[id] = append(m[id], v)
+				return nil
+			},
+	)
+	if err != nil{
+		return nil, err
+	}
+	return m, nil
+}
 
-func GroupAsSlice[T, I comparable](it Iterator[T],groupGetter func(v T) (I) )([][]T,error){
+
+func GroupAsSlice[T any, I comparable](it Iterator[T],groupGetter func(v T) (I) )([][]T,error){
 	groupAsMap,err := GroupAsMap(it,groupGetter)
 	if err != nil{
 		return nil, err
